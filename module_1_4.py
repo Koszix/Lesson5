66a33ece3ea5a70588f64b8a6aa6cd925b0fb37f
Koszix <koksyak69@gmail.com>
2024-08-30 07:48:36 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+my_string = ('Привет, меня зовут Константин! А тебя?')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+
+name = input('Введите имя: ')
+print('Приятно познакомиться,', name.upper(), "<3")
+# ^.^
